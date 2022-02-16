c6be3fece268e110f819e2aa2fa308568ce9b986
Artem Gusev <gusev2014russia@mail.ru>
2022-02-16 21:07:44 +0300

    Resolves "linked list cycle" task

---
+++ leetcode/algorithms/easy/linked-list-cycle/index.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (!head) {
+        return false;
+    }
+
+    let fast = head;
+    let slow = fast;
+
+    while (fast.next && fast.next.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (fast === slow) {
+            return true
+        }
+    }
+
+    return false;
+};
