195ef5b2d88c3197c980347a1a3438cd4daa997c
Artem Gusev <corocoto.art@gmail.com>
2023-01-07 13:27:16 +0300

    Adds third solution for "Remove Element" task

---
+++ leetcode/algorithms/easy/remove-element/thirdSolution.js
@@ -0,0 +1,18 @@
+// Link on the task: https://leetcode.com/problems/remove-element/description/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
