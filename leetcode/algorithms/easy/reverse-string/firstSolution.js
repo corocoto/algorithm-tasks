2d6644ab4a936a1bedbe4bff43f4740d109f30dc
Artem Gusev <corocoto.art@gmail.com>
2023-01-07 11:22:03 +0300

    Adds first solution for "Reverse String" task

---
+++ leetcode/algorithms/easy/reverse-string/firstSolution.js
@@ -0,0 +1,9 @@
+// Link on the task: https://leetcode.com/problems/reverse-string/description/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    return s.reverse();
+};
