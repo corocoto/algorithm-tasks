aa314ccc979f8670560bd577e9e2fa9505bd9490
Artem Gusev <corocoto.art@gmail.com>
2023-08-24 22:32:46 +0300

    Adds solution onto "Find the Maximum Achievable Number" task

---
+++ leetcode/algorithms/easy/find-the-maximum-achievable-number/index.js
@@ -0,0 +1,10 @@
+// Link on the task: https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num + t * 2;
+};
