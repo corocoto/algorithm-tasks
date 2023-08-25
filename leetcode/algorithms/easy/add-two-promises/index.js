fcedafbdf242442b007b6ab9eca55ba9abfc48e5
Artem Gusev <corocoto.art@gmail.com>
2023-08-25 09:20:54 +0300

    Adds solution onto "Add Two Promises" task

---
+++ leetcode/algorithms/easy/add-two-promises/index.js
@@ -0,0 +1,16 @@
+// Link on the task: https://leetcode.com/problems/add-two-promises/description/
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const nums = await Promise.all([promise1, promise2]);
+    return nums.reduce((acc, num) => acc + num);
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
