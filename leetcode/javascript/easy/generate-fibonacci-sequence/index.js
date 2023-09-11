e117170a25bd10025a6bcd4b643a632f5cc8c061
Artem Gusev <corocoto.art@gmail.com>
2023-09-11 09:32:54 +0300

    Adds solution onto "Generate Fibonacci Sequence" task

---
+++ leetcode/javascript/easy/generate-fibonacci-sequence/index.js
@@ -0,0 +1,20 @@
+// Link on the task: https://leetcode.com/problems/generate-fibonacci-sequence/description/
+
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let current = 0;
+    let next = 1;
+
+    while (true) {
+        yield current;
+        [current, next] = [next, current + next]
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
