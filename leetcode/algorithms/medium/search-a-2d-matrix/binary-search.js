507d03478054be85d31e8f5bddd997abd2c94bba
Artem Gusev <corocoto.art@gmail.com>
2023-12-26 22:49:27 +0300

    Adds solution onto the "Search a 2D Matrix" task via binary search

---
+++ leetcode/algorithms/medium/search-a-2d-matrix/binary-search.js
@@ -0,0 +1,35 @@
+// Link on the task: https://leetcode.com/problems/search-a-2d-matrix/description/
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    let left = 0;
+    let right = m * n - 1;
+
+    
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const midColumn = Math.floor(mid / n);
+        const midRow = Math.floor(mid % n);
+
+        if(matrix[midColumn][midRow] === target) {
+            return true
+        }
+
+        if (matrix[midColumn][midRow] > target) {
+            right = mid - 1;
+        }
+
+        if (matrix[midColumn][midRow] < target) {
+            left = mid + 1;
+        }
+    }
+
+    return false;
+};
